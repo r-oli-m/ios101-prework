4dd792fa1f6f6df13d9baf671622a3a5123a0657
Reva Mahto <reva.mahto@gmail.com>
2024-02-03 18:19:57 -0800

    boba stuff

--- ViewController.swift
+++ ViewController.swift
@@ -8,25 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func changeColor() -> UIColor{
 
-    @IBAction func changeBackgroundColor(_ sender: UIButton) {
-        func changeColor() -> UIColor{
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
 
-                let red = CGFloat.random(in: 0...1)
-                let green = CGFloat.random(in: 0...1)
-                let blue = CGFloat.random(in: 0...1)
-
-                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
-            }
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    @IBAction func changeBackgroundColor(_ sender: UIButton){
         let randomColor = changeColor()
-        view.backgroundColor = randomColor
+        view1.backgroundColor = randomColor
+        view2.backgroundColor = randomColor
     }
-    
-    
 }
 
